cc7e3234b930d5c44b8b7d2394c4f8cb0e0dd2aa
clukhei <kheiboardwarrior@gmail.com>
2023-10-04 16:25:55 +0800

    initial commit'

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import Footer from './components/Footer.vue'
+import "@govtechsg/sgds-web-component"
+import Mainnav from './components/Mainnav.vue'
+</script>
+
+<template>
+  <Mainnav />
+  <body>
+   <sgds-template-grid>
+    <sgds-content-area>
+      <sgds-content-body>
+        Hello world
+      </sgds-content-body>
+    </sgds-content-area>
+   </sgds-template-grid>
+  </body>
+  <Footer/>
+</template>
+
+<style scoped>
+body {
+  display: flex;
+  min-height: 80vh;
+  flex-direction: column;
+}
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,148 @@
+<template>
+  <div>
+  <sgds-footer :title="footerAttr.title" :description="footerAttr.description" :lastUpdatedDate="footerAttr.date" .:links="footerAttr.links"></sgds-footer>
+  </div>
+  </template>
+  
+  <script lang='ts'>
+  import { computed } from "vue";
+  
+  export default {
+    setup() {
+      const footerAttr = computed(() => ({
+        title: "Singapore Design System",
+        description: "this is a description",
+        date: new Date().toDateString(),
+        links : [
+          {
+              "title": "Column 1",
+              "links" : [
+                  {
+                      "href": "#1",
+                      "label": "About Us"
+                  },
+                  {
+                      "href": "#2",
+                      "label": "This is a super long link"
+                  },
+                  {
+                      "href": "#3",
+                      "label": "Test"
+                  },
+                  {
+                      "href": "#4",
+                      "label": "Test"
+                  }
+              ]
+          },
+          {
+              "title": "Column 2",
+              "links" : [
+                  {
+                      "href": "#1",
+                      "label": "About Us"
+                  },
+                  {
+                      "href": "#2",
+                      "label": "This is a super long link"
+                  },
+                  {
+                      "href": "#3",
+                      "label": "Test"
+                  },
+                  {
+                      "href": "#4",
+                      "label": "Test"
+                  }
+              ]
+          },
+          {
+              "title": "Column 3",
+              "links" : [
+                  {
+                      "href": "#1",
+                      "label": "About Us"
+                  },
+                  {
+                      "href": "#2",
+                      "label": "This is a super long link"
+                  },
+                  {
+                      "href": "#3",
+                      "label": "Test"
+                  },
+                  {
+                      "href": "#4",
+                      "label": "Test"
+                  }
+              ]
+          },
+          {
+              "title": "Column 4",
+              "links" : [
+                  {
+                      "href": "#1",
+                      "label": "About Us"
+                  },
+                  {
+                      "href": "#2",
+                      "label": "This is a super long link"
+                  },
+                  {
+                      "href": "#3",
+                      "label": "Test"
+                  },
+                  {
+                      "href": "#4",
+                      "label": "Test"
+                  }
+              ]
+          },
+          {
+              "title": "Column 5",
+              "links" : [
+                  {
+                      "href": "#1",
+                      "label": "About Us"
+                  },
+                  {
+                      "href": "#2",
+                      "label": "This is a super long link"
+                  },
+                  {
+                      "href": "#3",
+                      "label": "Test"
+                  },
+                  {
+                      "href": "#4",
+                      "label": "Test"
+                  }
+              ]
+          },
+          {
+              "title": "Column 6",
+              "links" : [
+                  {
+                      "href": "#1",
+                      "label": "About Us"
+                  },
+                  {
+                      "href": "#2",
+                      "label": "This is a super long link"
+                  },
+                  {
+                      "href": "#3",
+                      "label": "Test"
+                  },
+                  {
+                      "href": "#4",
+                      "label": "Test"
+                  }
+              ]
+          }
+        ]
+      }))
+      return { footerAttr };
+    },
+  };
+  </script>
